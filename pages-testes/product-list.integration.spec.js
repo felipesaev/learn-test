7871fd6e7d4b23cd23de8bc25dba07cbe5c384c4
felipecosta <felipesaev@gmail.com>
2022-05-04 08:43:50 -0300

    feat: create hook for product card and tests

--- pages-testes/product-list.integration.spec.js
+++ pages-testes/product-list.integration.spec.js
@@ -1,11 +1,36 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages'
+import { makeServer } from '../miragejs/server';
+
+const renderProductList = () => {
+  render(<ProductList />)
+  
+}
 
 
 describe('Product List', () => {
-  it('should render ProductList', () => {
-    render(<ProductList />)
+  let server;
 
+  beforeEach(() => {
+    server = makeServer({environment: 'test'})
+  })
+
+  afterEach(() => {
+    server.shutdown();
+  })
+  it('should render ProductList', () => {
+    renderProductList()
     expect(screen.getByTestId('product-list')).toBeInTheDocument()
   });
+  it('should render the ProductCard component 10 times', async () => {
+    server.createList('product', 10)
+    renderProductList()
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(10)
+
+    })
+    
+  });
+  
 });
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { useFetchProducts } from '../hooks/use-fetch-products';
 import ProductCard from '../components/product-card';
 import Search from '../components/search';
+import { makeServer } from '../miragejs/server';
+
+makeServer();
 
 export default function Home() {
+  const { products, error } = useFetchProducts();
+
   return (
     <main className="my-8" data-testid="product-list">
       <Search />
@@ -9,7 +15,9 @@ export default function Home() {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-        <ProductCard product={{}} />
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
         </div>
       </div>
     </main>
